a3f764e8ad52a31c589eff0d50c23f3326b95476
Globey163 <globey163@gmail.com>
2018-11-23 15:06:09 +0100

    Mappings

--- app/src/main/java/tiago/ninemenmorris/model/Board.java
+++ app/src/main/java/tiago/ninemenmorris/model/Board.java
@@ -35,8 +35,27 @@ public class Board {
 
         if (adjacencyMap == null) {
             adjacencyMap = new Hashtable<>();
+
+            //Horisonatl mappings
             map(Position.A1, HORIZONTAL, Position.D1, Position.G1);
+            map(Position.B2, HORIZONTAL, Position.D2, Position.F2);
+            map(Position.C3, HORIZONTAL, Position.D3, Position.E3);
+            map(Position.A4, HORIZONTAL, Position.B4, Position.C4);
+            map(Position.E4, HORIZONTAL, Position.F4, Position.G4);
+            map(Position.C5, HORIZONTAL, Position.D5, Position.E5);
+            map(Position.B6, HORIZONTAL, Position.D6, Position.F6);
+            map(Position.A7, HORIZONTAL, Position.D7, Position.G7);
+
+            //Vertical mappings
             map(Position.A1, VERTICAL, Position.A4, Position.A7);
+            map(Position.B2, VERTICAL, Position.B4, Position.B6);
+            map(Position.C3, VERTICAL, Position.C4, Position.C5);
+            map(Position.D1, VERTICAL, Position.D2, Position.D3);
+            map(Position.D5, VERTICAL, Position.D6, Position.D7);
+            map(Position.E3, VERTICAL, Position.E4, Position.E5);
+            map(Position.F2, VERTICAL, Position.F4, Position.F6);
+            map(Position.G1, VERTICAL, Position.G4, Position.G7);
+
         }
     }
 
