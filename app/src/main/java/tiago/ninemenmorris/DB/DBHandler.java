ab537a9a6bfadc12bb0acfd4d3399326696b3a0a
Tiago Redaelli <tiagor@kth.se>
2018-11-29 01:45:20 +0100

    Loading old games now work.

--- app/src/main/java/tiago/ninemenmorris/DB/DBHandler.java
+++ app/src/main/java/tiago/ninemenmorris/DB/DBHandler.java
@@ -152,7 +152,16 @@ public abstract class DBHandler extends RoomDatabase {
         } else {
             game.setCurrentPlayer(game.player2);
         }
+        /* TODO: Save game id as last played */
         return true;
+    }
+
+    /**
+     * Loads the last saved session if it's unfinished
+     */
+    public boolean loadLastSavedGameState() {
+        int id = 0; /* TODO Load last id */
+       return loadGame(id);
     }
 
     /**
@@ -169,85 +178,6 @@ public abstract class DBHandler extends RoomDatabase {
         return list;
     }
 
-
-
-    //method for saving into the database
-    /*
-    public void save(Game game) {
-
-        if(!game.isGameOver()){
-
-            GameEntity gameEntity = new GameEntity(game.isGameOver());
-            PlayerEntity player1Entity = new PlayerEntity(game.player1.name,
-                    game.player1.color.toString(),
-                    game.player1.getStateString(),
-                    1,
-                    game.player1.activeTurn,
-                    gameEntity.id);
-
-            PlayerEntity player2Entity = new PlayerEntity(game.player2.name,
-                    game.player2.color.toString(),
-                    game.player2.getStateString(),
-                    2,
-                    game.player2.activeTurn,
-                    gameEntity.id);
-
-            dbHandler.gameDAO().insertGame(gameEntity);
-            dbHandler.playerDAO().insertPlayer(player1Entity);
-            dbHandler.playerDAO().insertPlayer(player2Entity);
-            List<Checker> checkerList = game.getCheckers();
-            for (Checker c : checkerList){
-                CheckerEntity checkerEntity = new CheckerEntity(c.getColor().toString(),
-                        c.isDraggable(),
-                        c.getPosition().toString(),
-                        gameEntity.id);
-                dbHandler.checkerDAO().insertChecker(checkerEntity);
-            }
-        }
-    }
-    */
-
-    /*
-    public void load(Game game){
-        List<GameEntity> theGame;
-        List<PlayerEntity> thePlayers;
-        List<CheckerEntity> theCheckers;
-
-        try {
-            theGame = dbHandler.gameDAO().getGameById(game.getId());
-            game.setId(theGame.get(0).id);
-            game.setGameOver(theGame.get(0).gameOver);
-
-            thePlayers = dbHandler.playerDAO().getPlayersByGameId(game.getId());
-            if (thePlayers.get(0).playerTag == 1 && thePlayers.get(1).playerTag == 2){
-
-                game.player1.name = thePlayers.get(0).name;
-                game.player2.name = thePlayers.get(1).name;
-
-                game.player1.activeTurn = thePlayers.get(0).activeTurn;
-                game.player2.activeTurn = thePlayers.get(1).activeTurn;
-                if (game.player1.activeTurn){
-                    game.setCurrentPlayer(game.player1);
-                } else{
-                    game.setCurrentPlayer(game.player2);
-                }
-
-                game.player1.color = Color.valueOf(thePlayers.get(0).color);
-                game.player2.color = Color.valueOf(thePlayers.get(1).color);
-
-                game.player1.setState(Player.State.valueOf(thePlayers.get(0).state));
-                game.player2.setState(Player.State.valueOf(thePlayers.get(1).state));
-            }
-
-            theCheckers = dbHandler.checkerDAO().getcheckersByGameId(game.getId());
-            //more work needs to be done here.
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-*/
     //delete a game
     public void deleteGame() {
         Game game = Game.getInstance();
